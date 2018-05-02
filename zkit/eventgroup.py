85b9ff8e4f72c3ae6ae19c889d74c2bdf2350cd3
wkmanire <williamkmanire@gmail.com>
2018-05-02 00:19:15 -0700

    Enable sprites to publish events with EventGroup
    Sprites can now call EventGroup.publish(self, ...) to publish custom
    events into the current frame.
    
    Sprite event execution order can be controlled with the event_priority
    property. By default events will be executed in the inverse order they
    are published.
    
    Pygame events are handled before any sprite to sprite events in an
    effort to create predictability of behavior.

--- zkit/eventgroup.py
+++ zkit/eventgroup.py
@@ -1,5 +1,12 @@
 from pygame.sprite import Group
 
+from heapq import heappush
+from heapq import heappop
+
+INFINITE_UPDATE_MSG = ("Most impressive... the game has executed %s events in "
+                       "a single update! most likely an infinite loop has "
+                       "occured and this frame will NEVER END! MUA HA HA HA")
+
 
 def subscribe(event_key, **filter_kwargs):
     def decorator(handler):
@@ -31,20 +38,54 @@ they share the same handler qualified name."""
 
 class EventGroup(Group):
 
+    class PossibleInfiniteEventLoopError(Exception):
+        pass
+
+    MAX_SPRITE_EVENTS_PER_UPDATE = 100000
+
     def __init__(self, *sprites):
         super(EventGroup, self).__init__(*sprites)
         self.subscriptions_set = set()
         self.subscriptions = list()
+        self.sprite_events = list()
 
     def add(self, *sprites):
         super(EventGroup, self).add(*sprites)
         for sprite in sprites:
             self.bind_subscriptions(sprite)
 
+    def bind_subscriptions(self, sprite):
+        for key in dir(sprite):
+            value = getattr(sprite, key)
+            if self._is_event_handler_function(key, value):
+                self._bind_subscription(sprite, value)
+
+    def _is_event_handler_function(self, key, value):
+        return callable(value) and key.startswith("on_")
+
+    def _bind_subscription(self, sprite, subscription_factory):
+        subscription = subscription_factory()
+        if subscription not in self.subscriptions_set:
+            subscription.sprite = sprite
+            self.subscriptions_set.add(subscription)
+            self.subscriptions.append(subscription)
+
     def update(self, events, delta):
+        super(EventGroup, self).update(delta)
+        # first handle passed-in events (presumably pygame events)
         for event in events:
-            for subscription in self._get_matching_subscription(event):
-                subscription.execute_handler(event, delta)
+            self._handle_event(event, delta)
+
+        # now handle sprite published events until they are exhausted
+        # NOTE: It's possible to get into an infinite loop here
+        events_handled = 0
+        while self.sprite_events:
+            _, event = heappop(self.sprite_events)
+            self._handle_event(event, delta)
+            events_handled += 1
+            if events_handled > self.MAX_SPRITE_EVENTS_PER_UPDATE:
+                msg = INFINITE_UPDATE_MSG % events_handled
+                raise self.PossibleInfiniteEventLoopError(msg)
 
     def _get_matching_subscription(self, event):
         # Note that the event can either be a pygame event, or any type
@@ -63,18 +104,25 @@ class EventGroup(Group):
     def _filters_match(self, event, **filter_kwargs):
         return all(getattr(event, k) == v for k, v in filter_kwargs.items())
 
-    def bind_subscriptions(self, sprite):
-        for key in dir(sprite):
-            value = getattr(sprite, key)
-            if self._is_event_handler_function(key, value):
-                self._bind_subscription(sprite, value)
+    def _handle_event(self, event, delta):
+        for subscription in self._get_matching_subscription(event):
+            subscription.execute_handler(event, delta)
 
-    def _is_event_handler_function(self, key, value):
-        return callable(value) and key.startswith("on_")
+    @staticmethod
+    def publish(sprite, event):
+        for group in sprite.groups():
+            if isinstance(group, EventGroup):
+                group.publish_event(event)
 
-    def _bind_subscription(self, sprite, subscription_factory):
-        subscription = subscription_factory()
-        if subscription not in self.subscriptions_set:
-            subscription.sprite = sprite
-            self.subscriptions_set.add(subscription)
-            self.subscriptions.append(subscription)
+    def publish_event(self, event):
+        """Publish a sprite event to be handled during the current update.
+
+Sprite events are pushed into a priority heap. Event objects can set a
+event_priority attribute to cause themselves to be executed in an arbitrary
+order. The smaller the event_priority number, the higher priority the event
+is. When events have the same event_priority they will be executed in reverse
+insertion order. The default priority is assumed to be 99999
+
+        """
+        event_priority = getattr(event, "event_priority", 99999)
+        heappush(self.sprite_events, (event_priority, event))
--- zkit/test_eventgroup.py
+++ zkit/test_eventgroup.py
@@ -25,9 +25,39 @@ class TestSprite(Sprite):
 
 class TestEvent:
 
-    def __init__(self):
-        self.event_key = "some-key"
+    def __init__(self, key="some-key"):
+        self.event_key = key
         self.who = "some-value"
+
+
+class OtherSprite(Sprite):
+
+    def update(self, delta):
+        EventGroup.publish(self, TestEvent())
+
+
+class InfiniteEventSpriteA(Sprite):
+
+    def __init__(self, *groups):
+        Sprite.__init__(self, *groups)
+
+    def update(self, delta):
+        event = TestEvent(key="infinite-b")
+        EventGroup.publish(self, event)
+
+    @subscribe("infinite-a")
+    def on_infinite_a(self, event, delta):
+        EventGroup.publish(self, TestEvent(key="infinite-b"))
+
+
+class InfiniteEventSpriteB(Sprite):
+
+    def __init__(self, *groups):
+        Sprite.__init__(self, *groups)
+
+    @subscribe("infinite-b")
+    def on_infinite_a(self, event, delta):
+        EventGroup.publish(self, TestEvent(key="infinite-a"))
 
 
 class EventGroupTestCase(TestCase):
@@ -56,3 +86,15 @@ class EventGroupTestCase(TestCase):
         self.g.update([event], delta)
         self.assertIs(self.s.called_events[0][0], event)
         self.assertEqual(self.s.called_events[0][1], delta)
+
+    def test_sprites_can_publish_events(self):
+        self.g.add(OtherSprite())
+        self.g.update([], 0)
+        self.assertEqual(len(self.s.called_events), 1)
+
+    def test_detects_infinite_event_loop(self):
+        self.g.empty()
+        self.g.add(InfiniteEventSpriteA())
+        self.g.add(InfiniteEventSpriteB())
+        with self.assertRaises(EventGroup.PossibleInfiniteEventLoopError):
+            self.g.update([], 0)
